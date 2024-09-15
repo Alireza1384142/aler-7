2bd010796f490796fb041ab9c3c0e9528fb68738
Alireza1384142 <alireza.m.united.10@gmail.com>
2024-09-15 11:45:53 +0330

    Add files via upload

---
+++ aler7.py
@@ -0,0 +1,26 @@
+while True :
+
+    username = input('enter your username :')
+
+    if username == ('ali134') :
+
+        print('enter pass')
+
+    else :
+
+        print('try again')
+
+        break
+
+    password = input('enter password :')
+
+    if password == ('123456a') :
+
+        print('go forward')
+
+    else:
+
+        print('try again')
+
+    break
+    
